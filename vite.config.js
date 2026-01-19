9d0d86ae00839711f76426d165e5301fc7d88bde
jyothikachowdaryp <jyothikachowdarypamulapati@gmail.com>
2026-01-19 13:54:21 +0530

    Hosting fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-Website-Jyothika/", // Assuming this will be the repo name for GitHub Pages
+  base: "/Portfolio-Jyothika/", // Assuming this will be the repo name for GitHub Pages
 });
